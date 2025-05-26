65f7d4ab5f6918c7cb8f4460861b2550d47c0186
EagleUnit <130135604+EagleUnit@users.noreply.github.com>
2025-05-26 18:33:22 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,21 +1,20 @@
 Config = {}
 
 Config.Main = {
-    Framework = 'qb', -- 'qb' or 'esx'
-    Inventory = 'ox', -- 'qb' or 'esx' or 'ox'
-    Debug = true, -- Prints debug statements to the f8 console
-    CheckForUpdates = true -- Recommended to keep true to ensure your updated.
+    Framework = 'qb',          -- 'qb' or 'esx'
+    Inventory = 'ox',          -- 'qb' or 'esx' or 'ox'
+    Debug = true,              -- Prints debug statements to the f8 console
+    CheckForUpdates = true     -- Recommended to keep true to ensure your updated.
 }
 
 Config.Potion = {
     Teleport = true,            -- Enable the teleport
-    MaxPotionsPerPlayer = 5,    -- Limits how many potions a player can have active simultaneously
     RemoveAfterUse = true,      -- Remove the potion after using it. If false, keeps potion after use.
-    PotionCooldown = 15,         -- Time before a player can use another potion (in seconds)
+    PotionCooldown = 15,        -- Time before a player can use another potion (in seconds)
     TeleportDistance = 50,      -- How far you can teleport in metres. (1-xx randomized)
     TeleportSwap = true,        -- Make it so it can make you swap places with a nearby npc
     SwapRealPlayers = true,     -- If teleport swap is enabled, it will swap your place with a real player. If no players are nearby, it will swap with an npc
-    SwapChance = 100,            -- The chance that it will make you swap places with an npc or player
+    SwapChance = 100,           -- The chance that it will make you swap places with an npc or player
     UseAnimations = true,       -- Use animations for drinking the potion (Recommended)
     Animations = {
         EnterDrinkingAnim = {dict = 'mp_player_intdrink', anim = 'intro_bottle_fp'},
